b20966363fa2b1d76a9efdac2fa75682db31d7f5
BlazarKnight <152732605+BlazarKnight@users.noreply.github.com>
2025-03-27 12:47:03 -0400

    logo time

--- pakmandetect.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-for pm in apk app apt apt-get cargo dnf dpkg emerge eopkg flatpak nala moss nix-env pacman pamac portage rpm snap xbps yay yum zypper; do
-    echo | command -v $pm
-done
-exit 0
