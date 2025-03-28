961eefc6b9da493d64c7b9034c86a53fd947c004
BlazarKnight <152732605+BlazarKnight@users.noreply.github.com>
2025-03-28 19:46:08 -0400

    ui i need help but ill figur it out

--- frontend.py
+++ frontend.py
@@ -13,7 +13,7 @@ from kivy.app import App
 from kivy.uix.button import Button
 from kivy.core.window import Window
 from kivy.uix.textinput import TextInput
-
+from tkinter import filedialog
 from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.button import Button
@@ -22,6 +22,7 @@ from kivy.uix.label import Label
 global GLOB_BACK_GROUND_COLOR_AS_RGBA
 global GLOB_BORDER_COLOR_AS_RGBA
 global GLOB_TEXT_COLOR_AS_RGBA
+
 # set defalt color palet
 GLOB_BORDER_COLOR_AS_RGBA = [225,0,0,1] #red
 GLOB_TEXT_COLOR_AS_RGBA = [1,1,1,1] #white
@@ -38,8 +39,8 @@ GLOB_BACK_GROUND_COLOR_AS_RGBA = [0,0,0,1] #black
 class homeselect(App):
     def build(self):
         # Create a layout
-        layout = BoxLayout(orientation='vertical', spacing=10, padding=10)
-
+        layout = BoxLayout(orientation='vertical', spacing=20, padding=20)
+        self.butlay = BoxLayout(orientation='horizontal',spacing=20, padding=20)
         # Create a label
         #Textin = TextInput(text="PATH to file for backup replace this text", font_size='24sp')
         warn_lable= Label(text="input the path of folder you want to back up")
@@ -49,17 +50,28 @@ class homeselect(App):
         #textinput.bid(on_text_validate=on_enter)
 
         # Create a button
-        btn = Button(text="next", size_hint=(1, 0.2))
-        btn.bind(on_press=self.on_button_press)
+        btn1 = Button(text="next", size_hint=(1, 0.2))
+        btn2 = Button(text="choose a directory", size_hint=(1, 0.2))
+        btn2.bind(on_press=self.on_button2_press)
+        btn1.bind(on_press=self.on_button1_press)
 
         # Add widgets to the layout
         layout.add_widget(warn_lable)
-        layout.add_widget(self.textin)
-        layout.add_widget(btn)
+        #layout.add_widget(self.textin)
+
+        self.butlay.add_widget(self.textin)
+        self.butlay.add_widget(btn2)
+        layout.add_widget(self.butlay)
+        layout.add_widget(btn1)
+
 
         return layout
+    def on_button2_press(self,bnt2):
+        self.textin.insert_text(filedialog.askdirectory())
 
-    def on_button_press(self,btn ):
+
+
+    def on_button1_press(self, btn1):
         st.update_setting(key="home_folder",value=self.textin.text)
         print('The widget', self.textin.text, 'have:',self.textin.text)
         print(type(self.textin.text))
