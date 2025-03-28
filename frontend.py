dd0c51281cd1aed3b767db56a73ee2c9b2dd6d07
BlazarKnight <152732605+BlazarKnight@users.noreply.github.com>
2025-03-28 12:43:32 -0400

    big changes

--- frontend.py
+++ frontend.py
@@ -19,6 +19,21 @@ from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.button import Button
 from kivy.uix.label import Label
 
+global GLOB_BACK_GROUND_COLOR_AS_RGBA
+global GLOB_BORDER_COLOR_AS_RGBA
+global GLOB_TEXT_COLOR_AS_RGBA
+# set defalt color palet
+GLOB_BORDER_COLOR_AS_RGBA = [225,0,0,1] #red
+GLOB_TEXT_COLOR_AS_RGBA = [1,1,1,1] #white
+GLOB_BACK_GROUND_COLOR_AS_RGBA = [0,0,0,1] #black
+
+#this function is here so we dont need to write it again but dont use or impiment it untill kivy has full coloring suport for all used functions
+'''def theme():
+    print(bool(st.read_setting("dark_theme")))
+    if not  bool(st.read_setting("dark_theme")):
+        GLOB_TEXT_COLOR_AS_RGBA = [0,0,0,1] #black
+        GLOB_BACK_GROUND_COLOR_AS_RGBA = [1,1,1,1] #white
+    return 0'''
 
 class homeselect(App):
     def build(self):
@@ -27,7 +42,7 @@ class homeselect(App):
 
         # Create a label
         #Textin = TextInput(text="PATH to file for backup replace this text", font_size='24sp')
-
+        warn_lable= Label(text="input the path of folder you want to back up")
 
 
         self.textin= TextInput(multiline=False)
@@ -38,6 +53,7 @@ class homeselect(App):
         btn.bind(on_press=self.on_button_press)
 
         # Add widgets to the layout
+        layout.add_widget(warn_lable)
         layout.add_widget(self.textin)
         layout.add_widget(btn)
 
@@ -57,5 +73,7 @@ def runhomeselect():
 
 
 if __name__ == '__main__':
+    st.init_settings()
+    #theme()
     #TestApp().run()
     runhomeselect()
